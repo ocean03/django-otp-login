e1fe75bdc4cc0d5497af8d582049006f06b75844
JAHIR BHATTI <zaheerbhatti.pi@gmail.com>
2023-01-18 18:06:15 +0530

    fat: added email feature at signup

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+from authentication.helpers import send_email
 from authentication.models import User
 
 
@@ -17,6 +18,12 @@ class SignUpForm(UserCreationForm):
             'password1',
             'password2',
             ]
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        if commit:
+            send_email(user=user, email=user.email)
+        return user
 
 class LoginForm(forms.Form):
     mobile = forms.CharField(max_length=63)
